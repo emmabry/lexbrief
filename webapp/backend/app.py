2909ab2f4e57e71bde6504772475374e2f9a9d92
emma <emmaabryan@gmail.com>
2025-07-17 16:41:07 +0100

    update project plan, test eurlex-parser package for integrated document retrieval

--- webapp/backend/app.py
+++ webapp/backend/app.py
@@ -1,8 +1,23 @@
+from eurlex import get_data_by_celex_id, get_summary_by_celex_id, get_articles_by_celex_id
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "This is a test message!"}
+
+# Testing out packages for retrieving EUR-Lex data
+@app.get("/eurlex")
+async def eurlex():
+    data = get_data_by_celex_id('32013R0575')
+    print(data)
+    return data
+
+# Better for retrieving text
+@app.get("/eurlex_articles")
+async def eurlex_articles():
+    df = get_articles_by_celex_id('32013R0575')
+    print(df.head())
+    return df.text
