522e646fc2f86b9a76d2a3e32e59675d4ba6ab36
emma <emmaabryan@gmail.com>
2025-08-01 17:42:02 +0100

    fix imports for typescript
    Former-commit-id: 25f5edc82ea66c701da2f1e07fd197f9d3dc0ff6

---
+++ webapp/frontend/src/custom.d.ts
@@ -0,0 +1,12 @@
+// src/custom.d.ts
+declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+declare module '*.svg' {
+    const value: string;
+    export default value;
+  }
+  
+declare module '*.css';
